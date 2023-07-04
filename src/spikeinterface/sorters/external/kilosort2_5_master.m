3bd3be384ac240551aa3f69e6679cd4bba25ba65
Tom Bugnon <tombugnon@hotmail.com>
2023-07-04 12:20:25 -0500

    Remove double counted spikes

--- src/spikeinterface/sorters/external/kilosort2_5_master.m
+++ src/spikeinterface/sorters/external/kilosort2_5_master.m
@@ -98,7 +98,7 @@ function kilosort2_5_master(fpath, kilosortPath)
 
         % OPTIONAL: remove double-counted spikes - solves issue in which individual spikes are assigned to multiple templates.
         % See issue 29: https://github.com/MouseLand/Kilosort/issues/29
-        %rez = remove_ks2_duplicate_spikes(rez);
+        rez = remove_ks2_duplicate_spikes(rez, "overlap_s", 0.0001);
 
         % final merges
         rez = find_merges(rez, 1);
